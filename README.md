6e812dbeddeadbca8ca07fa2571a66f1886e487a
Gonçalo Rica Pais da Silva <bluefinger@gmail.com>
2023-03-21 16:39:54 +0100

    Separate README and lib docs

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The implementation is based on [wyhash](https://github.com/wangyi-fudan/wyhash),
 
 This crate can be used on its own or be integrated with `rand_core`/`rand`, and it is `no-std` compatible. Minimum compatible Rust version is 1.60.
 
-# Example
+## Example
 
 Generate a random value:
 
@@ -19,11 +19,19 @@ let mut rng = WyRand::new(Default::default());
 let value = rng.rand();
 ```
 
-# Features
+## Features
 
-The  crate will always export [`WyRand`] and will do so when set as `default-features = false` in the Cargo.toml. By default, it will have the `rand_core` & `debug` features enabled.
+The  crate will always export `WyRand` and will do so when set as `default-features = false` in the Cargo.toml. By default, it will have the `rand_core` & `debug` features enabled.
 
-* **`rand_core`** - Enables support for `rand_core`, implementing `RngCore` &
-  `SeedableRng` on [`WyRand`].
-* **`debug`** - Enables [`core::fmt::Debug`] implementation for [`WyRand`].
-* **`serde1`** - Enables `Serialize` and `Deserialize` derives on [`WyRand`].
+- **`rand_core`** - Enables support for `rand_core`, implementing `RngCore` & `SeedableRng` on `WyRand`.
+- **`debug`** - Enables `core::fmt::Debug` implementation for `WyRand`.
+- **`serde1`** - Enables `Serialize` and `Deserialize` derives on `WyRand`.
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,30 @@
-#![doc = include_str!("../README.md")]
+//! A fast & portable non-cryptographic pseudorandom number generator written in Rust.
+//!
+//! The implementation is based on [wyhash](https://github.com/wangyi-fudan/wyhash), a simple and fast hasher but **not** cryptographically secure. It's known to be extremely fast and performant while still having great statistical properties.
+//!
+//! This crate can be used on its own or be integrated with `rand_core`/`rand`, and it is `no-std` compatible. Minimum compatible Rust version is 1.60.
+//!
+//! # Example
+//!
+//! Generate a random value:
+//!
+//! ```rust
+//! use wyrand::WyRand;
+//!
+//! // Provide a seed to the PRNG
+//! let mut rng = WyRand::new(Default::default());
+//!
+//! let value = rng.rand();
+//! ```
+//!
+//! # Features
+//!
+//! The  crate will always export [`WyRand`] and will do so when set as `default-features = false` in the Cargo.toml. By default, it will have the `rand_core` & `debug` features enabled.
+//!
+//! * **`rand_core`** - Enables support for `rand_core`, implementing `RngCore` &
+//!   `SeedableRng` on [`WyRand`].
+//! * **`debug`** - Enables [`core::fmt::Debug`] implementation for [`WyRand`].
+//! * **`serde1`** - Enables `Serialize` and `Deserialize` derives on [`WyRand`].
 #![warn(missing_docs)]
 #![no_std]
 #[cfg(feature = "debug")]
@@ -13,6 +39,7 @@ use serde::{Deserialize, Serialize};
 /// A Pseudorandom Number generator, powered by the `wyrand` algorithm.
 #[derive(Clone, PartialEq)]
 #[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[repr(transparent)]
 pub struct WyRand {
     state: u64,
 }
