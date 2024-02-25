356f58845a91984396b3d3baaaf2740256a7e5e3
Gonçalo Rica Pais da Silva <bluefinger@gmail.com>
2024-02-25 09:14:16 +0100

    chore: improve benchmark code

--- benches/rand_bench.rs
+++ benches/rand_bench.rs
@@ -36,30 +36,27 @@ fn wyhash_benchmark(c: &mut Criterion) {
 
     use wyrand::WyHash;
 
-    #[rustfmt::skip]
-    let test_cases: [(u64, &str); 7] = [
-        (0x0409_638e_e2bd_e459, ""),
-        (0xa841_2d09_1b5f_e0a9, "a"),
-        (0x32dd_92e4_b291_5153, "abc"),
-        (0x8619_1240_89a3_a16b, "message digest"),
-        (0x7a43_afb6_1d7f_5f40, "abcdefghijklmnopqrstuvwxyz"),
-        (0xff42_329b_90e5_0d58, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"),
-        (0xc39c_ab13_b115_aad3, "12345678901234567890123456789012345678901234567890123456789012345678901234567890"),
+    let test_cases: [&str; 7] = [
+        "",
+        "a",
+        "abc",
+        "message digest",
+        "abcdefghijklmnopqrstuvwxyz",
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        "12345678901234567890123456789012345678901234567890123456789012345678901234567890",
     ];
 
     test_cases
         .into_iter()
         .enumerate()
-        .for_each(|(seed, (hash, input))| {
-            let test_name = format!("Hash message of length: {}", input.len());
-
-            c.bench_function(&test_name, |b| {
+        .for_each(|(seed, input)| {
+            c.bench_function(&format!("Hash message of length: {}", input.len()), |b| {
                 b.iter(|| {
-                    let mut hasher = black_box(WyHash::new_with_default_secret(seed as u64));
+                    let mut hasher = WyHash::new_with_default_secret(black_box(seed as u64));
 
-                    hasher.write(input.as_bytes());
+                    hasher.write(black_box(input.as_bytes()));
 
-                    assert_eq!(hash, hasher.finish())
+                    hasher.finish()
                 });
             });
         });
