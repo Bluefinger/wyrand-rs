bd63685af6a9f1a505328b70cb4bc5365923a6d1
Gonçalo Rica Pais da Silva <bluefinger@gmail.com>
2024-03-22 12:25:32 +0100

    refactor: Clean up prime code and consume bytes method

--- src/hasher/read.rs
+++ src/hasher/read.rs
@@ -1,5 +1,5 @@
 #[inline(always)]
-pub(super) const fn wyread64(bits: &[u8]) -> u64 {
+pub(super) const fn read_8_bytes(bits: &[u8]) -> u64 {
     (bits[7] as u64) << 56
         | (bits[6] as u64) << 48
         | (bits[5] as u64) << 40
@@ -11,12 +11,12 @@ pub(super) const fn wyread64(bits: &[u8]) -> u64 {
 }
 
 #[inline(always)]
-pub(super) const fn wyread32(bits: &[u8]) -> u64 {
+pub(super) const fn read_4_bytes(bits: &[u8]) -> u64 {
     (bits[3] as u64) << 24 | (bits[2] as u64) << 16 | (bits[1] as u64) << 8 | (bits[0] as u64)
 }
 
 #[inline(always)]
-pub(super) const fn wyread_upto_24(bits: &[u8]) -> u64 {
+pub(super) const fn read_upto_3_bytes(bits: &[u8]) -> u64 {
     (bits[0] as u64) << 16 | (bits[bits.len() >> 1] as u64) << 8 | (bits[bits.len() - 1] as u64)
 }
 
